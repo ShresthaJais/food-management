a39b9c508d83e4c6685e160fe47266885a97272a
ShresthaJais <jaiswalshrestha257@gmail.com>
2024-07-01 16:59:55 +0530

    your msg

---
+++ login.js
@@ -0,0 +1,5 @@
+document.querySelector('.login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your form validation or submission code here
+    alert('Form submitted');
+});
